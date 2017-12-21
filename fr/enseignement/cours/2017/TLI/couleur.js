30cdc11bd2dea2e8323a1ff566ae9c6fc1b1faa2
John Samuel <johnsamuelwrites@gmail.com>
2017-12-21 15:31:06 +0100

    Ajouter exemple sur les couleurs

---
+++ fr/enseignement/cours/2017/TLI/couleur.js
@@ -0,0 +1,29 @@
+var id = document.getElementById("imageData");
+var context = id.getContext("2d");
+var img = new Image();
+img.src = "rose.bmp";
+context.drawImage(img, 200, 0, 400, 600);
+
+function imageData(r, g, b) {
+  context.clearRect(0, 0, 400, 600)
+  context.drawImage(img, 200, 0, 400, 600);
+  var data = context.getImageData(200, 0, 400, 600);
+  for(i=0; i< data.data.length; i+=3)
+    if(data.data[i] > r - 20  && data.data[i] < r + 20 &&
+       data.data[i+1] > g - 20 && data.data[i + 1] < g + 20 &&
+       data.data[i+2] > b - 20 && data.data[i + 1] < b + 20) {
+      data.data[i]=205;
+      data.data[i+1]=205;
+      data.data[i+2]=205;
+    }
+  context.putImageData(data, 200, 0);
+}
+
+
+function imageCouleur() {
+  var input = document.getElementById("couleur");
+  var r = parseInt(input.value.substr(1,2), 16);
+  var g = parseInt(input.value.substr(3,2), 16);
+  var b = parseInt(input.value.substr(5,2), 16);
+  imageData(r, g, b);
+}
