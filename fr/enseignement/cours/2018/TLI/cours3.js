9c7380512a61643fb2cedae5f019cabafeb1736f
John Samuel <johnsamuelwrites@gmail.com>
2019-04-24 12:22:30 +0200

    Update CSS and separate functions and calls

---
+++ fr/enseignement/cours/2018/TLI/cours3.js
@@ -0,0 +1,51 @@
+image();
+imageRotate();
+imageScale();
+imageTranslate();
+imageTransform();
+
+cercle("introduction", false);
+rectangle("introduction", false, false);
+moncanvastexte();
+
+ligne("ligne1", true);
+ligne("ligne2", false);
+ligne3();
+
+lignes1();
+lignes2();
+
+arc1();
+arc2();
+arc3("arc3", false);
+arc3("arc4", true);
+courbequadratique("courbe1", false);
+courbequadratique("courbe2", true);
+
+cercle("cercle");
+ellipse1("ellipse1");
+ellipse2("ellipse2");
+rectangle("rectangle1", false);
+rectangle("rectangle2", true);
+triangle("triangle1", false);
+triangle("triangle2", true);
+
+hexagone("hexagone1", false);
+hexagone("hexagone2", true);
+
+effacer("clearrect");
+
+bonjour("texte1", false);
+bonjour("texte2", true);
+
+pointeur("pointeur");
+alignement("alignement");
+base("alignementbase");
+
+image();
+imagezone();
+imagezone2();
+imagedecoupage();
+
+moncanvas1();
+moncanvas2();
