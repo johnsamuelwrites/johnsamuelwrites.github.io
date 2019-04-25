d4949e634221a673c5e7c4cd71559388a8524b93
John Samuel <johnsamuelwrites@gmail.com>
2019-04-25 14:01:28 +0200

    Add style and template

--- src/main/template.py
+++ src/main/template.py
@@ -3,7 +3,7 @@
 
 '''
   This will take one or more input HTML files and a single CSS file.
-  Content between <style> and </style> will be replaced by the contents 
+  Content between <template> and </template> will be replaced by the contents 
   of the CSS file
 '''
 
@@ -12,23 +12,64 @@ from shutil import copy
 from os import remove
 import re
 
-def change_style(filename, stylefilepath):
+def modify_content(filename):
   outputfile = open("/tmp/temp.html", "w") 
 
-  #Setting up regular expression for style
+  #Setting up regular expression for template
+  pattern = r'(<div class="title">(\n|.)*?</div>(\n|.)*?<div class="subtitle">(\n|.)*?</div>)'
+ 
+  content = ""
+  template = r'<div id="sidebar">\n      \1\n      </div>'
+  with open(filename,"r") as inputfile:
+    content = inputfile.read()
+  content = re.sub(pattern, template, content)
+  outputfile.write(content)
+
+  outputfile.close()
+  inputfile.close()
+
+  #Replacing the old file
+  remove(filename)
+  copy("/tmp/temp.html", filename)
+  remove("/tmp/temp.html")
+
+def remove_content(filename, patterns):
+  outputfile = open("/tmp/temp.html", "w") 
+
+  content = ""
+  template = ""
+  with open(filename,"r") as inputfile:
+    content = inputfile.read()
+  for pattern in patterns:
+    content = re.sub(pattern, template, content)
+
+  outputfile.write(content)
+
+  outputfile.close()
+  inputfile.close()
+
+  #Replacing the old file
+  remove(filename)
+  copy("/tmp/temp.html", filename)
+  remove("/tmp/temp.html")
+
+def add_content(filename, templatefilepath):
+  outputfile = open("/tmp/temp.html", "w") 
+
+  #Setting up regular expression for template
   pattern = r'<div class="home">(\n|.)*?</div>'
  
   content = ""
-  style = ""
+  template = ""
   with open(filename,"r") as inputfile:
     content = inputfile.read()
-  with open(stylefilepath,"r") as stylefile:
-    style = stylefile.read()
-  style = '<div class="home">\n'+ style + "\n      </div>"
-  content = re.sub(pattern, style, content)
+  with open(templatefilepath,"r") as templatefile:
+    template = templatefile.read()
+  template = '<div class="home">\n'+ template + "\n      </div>"
+  content = re.sub(pattern, template, content)
   outputfile.write(content)
 
-  stylefile.close()
+  templatefile.close()
   outputfile.close()
   inputfile.close()
 
@@ -39,10 +80,15 @@ def change_style(filename, stylefilepath):
 
 def modify_files(files):
   inputfiles = files[:len(files)-1]
-  stylefile = files[len(files)-1]
+  templatefile = files[len(files)-1]
   for filename in inputfiles:
-    print(filename, stylefile)
-    change_style(filename, stylefile)
+    print(filename, templatefile)
+    patterns = [r'<div class="licence">(\n|.)*?</div>',
+               r'<div class="separator" id="top">(\n|.)*?</div>',
+               r'<div class="separator" id="bottom">(\n|.)*?</div>']
+    add_content(filename, templatefile)
+    remove_content(filename, patterns)
+    modify_content(filename)
   
 
 parser = argparse.ArgumentParser(description='change section of HTML file')
@@ -51,7 +97,7 @@ parser.add_argument('files', metavar='F', type=str, nargs='+',
 
 args = parser.parse_args()
 
-# style file and HTML file
+# template file and HTML file
 if(len(args.files) < 2):
   parser.print_usage()
   exit(1)
