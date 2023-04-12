203f3ecd0ef7b34857c797ebfca3b8b72ea8f882
John Samuel <johnsamuelwrites@gmail.com>
2023-04-12 10:25:28 +0200

    Ajouter matériel de référence pour le cours

---
+++ fr/enseignement/cours/référence/TLI/cours3.js
@@ -0,0 +1,59 @@
+image();
+imageRotate();
+imageScale();
+imageTranslate();
+imageTransform();
+
+cercle("introduction", false);
+rectangle("introduction", false, false);
+moncanvastexte();
+
+ligne("ligne1", true);
+ligne("ligne2", false);
+ligne3();
+ligne4("lignes4", false);
+ligne4("lignes5", true);
+
+lignes1();
+lignes2();
+
+arc1();
+arc2();
+arc3("arc3", false);
+arc3("arc4", true);
+courbequadratiquebezier("courbe1", false);
+courbequadratiquebezier("courbe2", true);
+
+cercle("cercle");
+ellipse1("ellipse1");
+ellipse2("ellipse2");
+rectangle("rectangle1", false);
+rectangle("rectangle2", true);
+triangle("triangle1", false);
+triangle("triangle2", true);
+fillStyleRectangle("fillStyleRectangle");
+fillStyleRectangleTransparence("fillStyleRectangleTransparence");
+fillStyleRectangleGradient("fillStyleRectangleGradient");
+fillStyleRectangleRadialGradient("fillStyleRectangleRadialGradient");
+
+hexagone("hexagone1", false);
+hexagone("hexagone2", true);
+
+effacer("clearrect");
+
+bonjour("texte1", false);
+bonjour("texte2", true);
+
+pointeur("pointeur");
+alignement("alignement");
+base("alignementbase");
+
+image();
+imagezone();
+imagezone2();
+imagedecoupage();
+
+moncanvas1();
+moncanvas2();
+path2dcercle("path2dstroke", true, true);
+path2dcercle("path2dfill", false, true);
